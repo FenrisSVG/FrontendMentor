4e180240eac7bfa452982cd3550d0b82f58d4c1e
Fenris <fsandovalchavez5@gmail.com>
2022-01-31 19:54:25 -0600

    Frontend Mentor newbie challenge testing tailwind

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+  name: 'HelloWorld'
+</script>
+
+<template>
+  <div class="max-w-sm bg-white rounded-lg p-4 lg:rounded-2xl lg:p-5">
+    <div class="rounded-xl overflow-hidden">
+      <img src="../assets/desktop/image-qr-code.png" alt="qr code image"
+      class="block max-w-full" />
+    </div>
+    <div class="text-center pb-5">
+      <h1 class="text-[1.7rem] my-5 font-bold leading-tight lg:my-4">Improve your front-end skills by building projects</h1>
+      <p class="text-slate-400 lg:px-6">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  </div>
+</template>
+
+
+
